58c84a9109793e5d296b5c53ec1616e75b7c2bb8
ahnai17 <ahnai17@student.sdu.dk>
2023-11-23 08:26:01 +0100

    Update taxi.R

--- taxi.R
+++ taxi.R
@@ -23,11 +23,11 @@ taxi_data <- read_parquet('https://d37ci6vzurychx.cloudfront.net/trip-data/yello
 taxi_zones <- read_csv("https://d37ci6vzurychx.cloudfront.net/misc/taxi+_zone_lookup.csv") # nolint
 
 # Calculate points at which to plot labels (https://stackoverflow.com/a/50860504/9698208) # nolint
-centroids <- taxi_shp %>% 
-  st_centroid() %>% 
+centroids <- taxi_shp %>%
+  st_centroid() %>%
   bind_cols(as_data_frame(st_coordinates(.)))
 
-  # Define UI for application
+# Define UI for application
 ui <- fluidPage(
   titlePanel("Taxi Fares Map"),
   sidebarLayout(
@@ -45,32 +45,41 @@ server <- function(input, output) {
   output$taxiPlot <- renderPlot({
     # Calculate the total amount of money spent in each location
     total <- merge(
-      aggregate(taxi_data$total_amount, by=list(location_id=taxi_data$DOLocationID), FUN=sum),
-      aggregate(taxi_data$total_amount, by=list(location_id=taxi_data$PULocationID), FUN=sum),
-      by="location_id",
-      all.x=TRUE,
-      all.y=TRUE
+      aggregate(taxi_data$total_amount,
+                by = list(location_id = taxi_data$DOLocationID),
+                FUN = sum),
+      aggregate(taxi_data$total_amount,
+                by = list(location_id = taxi_data$PULocationID),
+                FUN = sum),
+      by = "location_id",
+      all.x = TRUE,
+      all.y = TRUE
     ) %>%
-    mutate_at(c('x.x','x.y'), ~replace_na(.,0)) %>%
-    mutate(money = x.x + x.y, x.x = NULL, x.y = NULL) %>%
-    mutate_at(c('location_id'), as.character) %>%
-    left_join(taxi_shp, ., by = c('location_id' = 'location_id'))
+      mutate_at(c("x.x", "x.y"), ~replace_na(., 0)) %>%
+      mutate(money = x.x + x.y, x.x = NULL, x.y = NULL) %>%
+      mutate_at(c("location_id"), as.character) %>%
+      left_join(taxi_shp, ., by = c("location_id" = "location_id"))
 
     # Plot the map
     ggplot() +
-    geom_sf(data = total, aes(group = location_id, fill = money), color = "white") +
-    scale_colour_distiller(palette="Reds", name="Frequency", guide = "colorbar") +
-    coord_sf() +
-    geom_text(aes(X, Y, label = location_id), data = centroids, size = 1) +
-    theme_void() +
-    scale_fill_gradient(
-      low = "yellow",
-      high = "red",
-      na.value = "grey50",
-      trans = "log10"
-    )
+      geom_sf(data = total,
+              aes(group = location_id,
+                  fill = money),
+              color = "white") +
+      scale_colour_distiller(palette = "Reds",
+                             name = "Frequency",
+                             guide = "colorbar") +
+      coord_sf() +
+      geom_text(aes(X, Y, label = location_id), data = centroids, size = 1) +
+      theme_void() +
+      scale_fill_gradient(
+        low = "yellow",
+        high = "red",
+        na.value = "grey50",
+        trans = "log10"
+      )
   })
 }
 
-# Run the application 
+# Run the application
 shinyApp(ui = ui, server = server)
