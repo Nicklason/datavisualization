a3a42e559e80c2e9fe3e80437080c1a5e8925cee
Nicklason <nicklas.marc.pedersen@gmail.com>
2023-12-06 14:11:27 +0100

    add tooltips and make plot interactive

--- taxi.R
+++ taxi.R
@@ -12,8 +12,12 @@ library(ggplot2)
 library(tidyr)
 # shiny app
 library(shiny)
-# Don't use scientific notation in the plot
-options(scipen = 999)
+# interactive ggplot
+library(plotly)
+# create intervals
+library(classInt)
+# string templating
+library(glue)
 
 # Read the geojson file
 taxi_shp <- read_sf('https://data.cityofnewyork.us/api/geospatial/d3c5-ddgc?method=export&format=GeoJSON') # nolint
@@ -42,7 +46,7 @@ ui <- fluidPage(
 
 # Define server logic
 server <- function(input, output) {
-  output$taxiPlot <- renderPlot({
+  output$taxiPlot <- renderPlotly({
     # Calculate the total amount of money spent in each location
     total <- merge(
       aggregate(taxi_data$total_amount, by=list(location_id=taxi_data$DOLocationID), FUN=sum),
@@ -56,19 +60,32 @@ server <- function(input, output) {
     mutate_at(c('location_id'), as.character) %>%
     left_join(taxi_shp, ., by = c('location_id' = 'location_id'))
 
-    # Plot the map
-    ggplot() +
-    geom_sf(data = total, aes(group = location_id, fill = money), color = "white") +
+    # Scale money down to not show scientific notation numbers
+    total$money_scaled = total$money / 100000
+    
+    # Create 7 intervals for the monies
+    breaks_qt <- classIntervals(c(0, total$money_scaled), n = 7, style = "quantile")
+
+    # Use the intervals
+    total <- mutate(total, money_cat = cut(money_scaled, breaks_qt$brks))
+
+    # Add label to each row
+    total <- total %>%
+      mutate(
+        model = rownames(.),
+        label = glue::glue('{zone} \nMoney spent: ${money}' )
+      )
+
+    # Create plot
+    p <- ggplot() +
+      geom_sf(data = total, aes(group = location_id, fill = money_cat, text = label), color = "white") +
     scale_colour_distiller(palette="Reds", name="Frequency", guide = "colorbar") +
     coord_sf() +
-    geom_text(aes(X, Y, label = location_id), data = centroids, size = 1) +
     theme_void() +
-    scale_fill_gradient(
-      low = "yellow",
-      high = "red",
-      na.value = "grey50",
-      trans = "log10"
-    )
+      scale_fill_brewer(palette = "OrRd")
+
+    # Make the plot interactive
+    ggplotly(p, tooltip = "text", height = 1000)
   })
 }
 
